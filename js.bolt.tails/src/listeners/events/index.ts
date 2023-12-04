52ca6dd4cf6d71e91b4d40a57f321ee141927e64
Ethan Zimbelman <ethan.zimbelman@me.com>
2023-12-04 10:39:32 -0800

    js.bolt.tails: respond to app_mention separately from message

--- js.bolt.tails/src/listeners/events/index.ts
+++ js.bolt.tails/src/listeners/events/index.ts
@@ -1,11 +1,13 @@
 import { App } from '@slack/bolt';
 
 import { appHomeOpened } from './app-home';
+import { appMention } from './app-mention';
 import { memberJoinedChannel, memberLeftChannel } from './channel-membership';
 import { reactionAdded } from './reaction-added';
 
 const register = (app: App) => {
     app.event('app_home_opened', appHomeOpened);
+    app.event('app_mention', appMention);
     app.event('member_joined_channel', memberJoinedChannel);
     app.event('member_left_channel', memberLeftChannel);
     app.event('reaction_added', reactionAdded);
