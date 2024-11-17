f017efe9f69da0b9b0f81a1a45ecf968a8b8e888
@zimeg <zim@o526.net>
2024-11-16 23:17:07 -0800

    chore(deps): bump the build tools for compilation to the latest

--- java.sdk.gibra/flake.nix
+++ java.sdk.gibra/flake.nix
@@ -16,13 +16,13 @@
             if pkgs.stdenv.isDarwin then
               pkgs.fetchurl
                 {
-                  url = "https://downloads.slack-edge.com/slack-cli/slack_cli_2.29.1_macOS_64-bit.tar.gz";
-                  sha256 = "1krzqkv9rsfxyhv4z7c2pa1hgjs73sb7ip2bnc7njl08yapnhavp";
+                  url = "https://downloads.slack-edge.com/slack-cli/slack_cli_2.31.0_macOS_64-bit.tar.gz";
+                  sha256 = "15xv5fpm7pdb0jyjniyky4vlafp4g0isnzxf23m7fynw2jkj7s1a";
                 }
             else
               pkgs.fetchurl {
-                url = "https://downloads.slack-edge.com/slack-cli/slack_cli_2.29.1_linux_64-bit.tar.gz";
-                sha256 = "054i8msn6y6k02dm9930l6nrm3i83axcz6wp4s8izri4kw9gklh3";
+                url = "https://downloads.slack-edge.com/slack-cli/slack_cli_2.31.0_linux_64-bit.tar.gz";
+                sha256 = "1bdk8c1insqhjkrn3fzc6dzlkgq6y0nvhac6rs3fsv81if8a5fgf";
               };
           unpackPhase = "tar -xzf $src";
           installPhase = ''
@@ -34,7 +34,7 @@
       {
         devShell = pkgs.mkShell {
           buildInputs = [
-            pkgs.jdk21 # https://github.com/openjdk/jdk/releases/tag/jdk-21-ga
+            pkgs.jdk23 # https://github.com/openjdk/jdk/releases/tag/jdk-23-ga
             pkgs.gradle # https://github.com/gradle/gradle
             slackcli
           ];
