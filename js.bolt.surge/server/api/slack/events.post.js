0583b1218e9322b2bea7736423ec228f6dc628e3
@zimeg <zim@o526.net>
2026-02-16 02:00:38 -0800

    fix: start

--- js.bolt.surge/server/api/slack/events.post.js
+++ js.bolt.surge/server/api/slack/events.post.js
@@ -7,11 +7,17 @@ const handler = createHandler(
   /** @type {import("@vercel/slack-bolt").VercelReceiver} */ (receiver),
 );
 
+let initialized = false;
+
 /**
  * Handle incoming Slack events via HTTP POST.
  * @param {import("h3").H3Event} event
  */
 export default defineEventHandler(async (event) => {
+  if (!initialized) {
+    await app.init();
+    initialized = true;
+  }
   const request = toWebRequest(event);
   return handler(request);
 });
